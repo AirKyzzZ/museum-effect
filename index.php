6a84bf1f410c21f403fa875257dc04d6e9089dae
Tyrdak <airkyzzz.jiji@gmail.com>
2024-11-19 08:50:47 +0100

    Ajouts fichiers et dossier

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil - Musée d'Aquitaine</title>
+</head>
+<body>
+    <header>
+    <?php include ("templates/template-parts/navbar.php") ?>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        <?php include ("templates/template-parts/footer.php")?>
+    </footer>
+</body>
+</html>
