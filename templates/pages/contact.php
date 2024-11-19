2d04b82891f8a337684adb90ff7f03dcfe144e57
AirKyzzZ <airkyzzz.jiji@gmail.com>
2024-11-19 09:36:03 +0100

    serveur

--- templates/pages/contact.php
+++ templates/pages/contact.php
@@ -9,21 +9,18 @@ session_start()
     <title>Contact - Musée d'Aquitaine</title>
 </head>
 <body>
-    <form action="traitement.php" method="POST">
-        <label for="name">Nom :</label>
-        <input type="text" id="name" name="name" required>
-        <label for="name">Prenom :</label>
-        <input type="text" id="prenom" name="prenom" required>
-        <label for="dte_naissance">Date de naissance :</label>
-        <input type="date" id="dte_naissance" name="dte_naissance" required>
-        <label for="email">Email :</label>
-        <input type="number" id="age" name="age" required>
-        <input type="email" id="email" name="email" required>
-        <label for="message">Message :</label>
-        <textarea id="message" name="message" required></textarea>
-        <button type="submit">Envoyer</button>
-        <p id="error-message"></p>
-        <p id="success-message"></p>
-    </form>
+<form action="traitement.php" method="POST">
+    <label for="name">Nom :</label>
+    <input type="text" id="name" name="name" required>
+    <label for="prenom">Prénom :</label>
+    <input type="text" id="prenom" name="prenom" required>
+    <label for="age">Âge :</label>
+    <input type="number" id="age" name="age" required>
+    <label for="email">Email :</label>
+    <input type="email" id="email" name="email" required>
+    <label for="message">Message :</label>
+    <textarea id="message" name="message" required></textarea>
+    <button type="submit">Envoyer</button>
+</form>
 </body>
 </html>
