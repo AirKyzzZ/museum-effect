4a447ed25a54c16596b0f5d5d54d6e25c1ae6cd2
AirKyzzZ <airkyzzz.jiji@gmail.com>
2024-11-20 19:46:42 +0100

    footer + font

--- templates/template-parts/footer.php
+++ templates/template-parts/footer.php
@@ -1,12 +1,14 @@
-<footer>
+
     <div class="social-media">
-        <a href="https://www.linkedin.com/in/maxime-mansiet-6b5934334/"><img src="images/linkedin.png" alt="LinkedIn"></a>
-        <a href="https://x.com/AirKyzzZ"><img src="images/twitter.png" alt="Twitter"></a>
-        <a href="https://www.instagram.com/maxx.height"><img src="images/instagram.png" alt="Instagram"></a>
+        <a href="https://www.linkedin.com/in/maxime-mansiet-6b5934334/"><img src="templates/images/facebook.png" alt="LinkedIn"></a>
+        <a href="https://x.com/AirKyzzZ"><img src="templates/images/twitter.png" alt="Twitter"></a>
+        <a href="https://www.instagram.com/maxx.height"><img src="templates/images/instagram.png" alt="Instagram"></a>
+        <a href="https://www.instagram.com/maxx.height"><img src="templates/images/rss.png" alt="RSS"></a>
     </div>
     <hr>
     <div class="contact">
-        <a href="mailto:maxime.mansiet@gmail.com">Contactez-moi</a>
+        <a href="#">Plan du site</a>
+        <a href="#">Mentions Legales</a>
+        <a href="mailto:maxime.mansiet@gmail.com">Mail</a>
     </div>
-    <p>&copy; 2024 - Maxime Mansiet. Tous droits réservés.</p>
-</footer>
+    <p>&copy; 2024 - Maxime Mansiet - Mael Bourdin. Tous droits réservés.</p>
