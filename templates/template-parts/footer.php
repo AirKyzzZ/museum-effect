2bc4631cae482ea39c6c33fedfecfdc230a4e975
AirKyzzZ <airkyzzz.jiji@gmail.com>
2024-11-20 18:51:28 +0100

    footer

---
+++ templates/template-parts/footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="social-media">
+        <a href="https://www.linkedin.com/in/maxime-mansiet-6b5934334/"><img src="images/linkedin.png" alt="LinkedIn"></a>
+        <a href="https://x.com/AirKyzzZ"><img src="images/twitter.png" alt="Twitter"></a>
+        <a href="https://www.instagram.com/maxx.height"><img src="images/instagram.png" alt="Instagram"></a>
+    </div>
+    <hr>
+    <div class="contact">
+        <a href="mailto:maxime.mansiet@gmail.com">Contactez-moi</a>
+    </div>
+    <p>&copy; 2024 - Maxime Mansiet. Tous droits réservés.</p>
+</footer>
