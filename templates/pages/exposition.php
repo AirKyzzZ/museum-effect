c8b5da609b9a0e192a9d07d1935fc58e5fa32645
AirKyzzZ <airkyzzz.jiji@gmail.com>
2024-12-03 16:35:39 +0100

    aled

---
+++ templates/pages/exposition.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Expositions</title>
+</head>
+<body>
+<header>
+        <?php include ("../templates-pages/navbar_page.php") ?>
+    </header>
+    <div class="content">
+        <h1>➔Le parcours 400 000 ans d'histoire(s)</h1><br>
+        <h3><i>Grâce à la richesse et à la variété de ses collections, le musée d'Aquitaine invite le visiteur à partir à la découverte de l'histoire régionale, de la préhistoire à l'époque contemporaine.</i></h3>
+    </div>
+    <footer>
+        <?php include ("../templates-pages/footer_page.php")?>
+    </footer>
+</body>
+</html>
